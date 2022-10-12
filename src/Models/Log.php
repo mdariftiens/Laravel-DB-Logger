43c742c2b2ae5cf5ae2e8d6e0e0cc19191d7a158
mdariftiens <md.arif.tiens@gmail.com>
2022-10-12 11:36:50 +0600

    added softdelete on log model

--- src/Models/Log.php
+++ src/Models/Log.php
@@ -4,9 +4,12 @@ namespace Mdariftiens\DBlog\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Log extends Model
 {
+    use SoftDeletes;
+
     /**
      * @var string $table
      */
